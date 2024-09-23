de6dd9104f2abd256af2e5c9324cf9cc11a62969
krystal0093 <krystalline0093@gmail.com>
2024-09-23 17:42:01 +0900

    240923 day01

--- Baekjoon/src/java/Day01.java
+++ Baekjoon/src/java/Day01.java
@@ -1,5 +1,8 @@
 package java;
 
 public class Day01 {
-
+	    public static void main(String[] args) {
+	        // main 메소드 실행 시, 아래 print 코드 실행
+	        System.out.println("Hello World!");
+	    }
 }
